2c228367b2efcef6edf89edced4c07a755f41969
kurtulus.akkaya <kurtulus.akkaya@felixo.com>
2022-05-17 14:53:55 +0300

    Connect to Server & Copy Files From Local To Remote

--- jMeterRemote.py
+++ jMeterRemote.py
@@ -5,17 +5,36 @@ from scp import SCPClient
 servers = config.servers
 passwords = config.passwords
 
+def connect_to_server(server,passw):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname=server, port=config.port, username=config.username, password=passw)
+    print("Server connection is open: "+server)
+    return ssh_client
+
+
+def copy_file_from_windows(connection,localPath,remotePath):
+    print(localPath+" ---------Transfer Started-------> "+remotePath)
+    with SCPClient(connection.get_transport()) as scp:
+        scp.put(files=localPath,remote_path=remotePath)
+    print(localPath+" ---------Transfer Done-------> "+remotePath)
+
+
 def control_installation():
     pass
+
 
 def run_test():
     pass
 
+
 def prep_users():
     pass
 
+
 def install():
     pass
+
 
 def main():
     
